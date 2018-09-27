ffdde52e11beef258b45ee74cbf7bb525a620c69
panchoruy <panchifoco@gmail.com>
2018-09-26 20:49:13 -0400

    Fixed couple of bugs, checked std peer thoroughly

--- ranchostd.py
+++ ranchostd.py
@@ -15,6 +15,7 @@ class RanchoStd(Peer):
     def post_init(self):
         self.upload_slots = 4
         self.optimistic_slots = 1
+        self.optimistically_unchoked_peer_id_list = [None]
 
     def requests(self, peers, history):
         """
@@ -45,7 +46,7 @@ class RanchoStd(Peer):
             pieces_by_holder_dict[(len(holder_peer_id_list), piece_id)] = holder_peer_id_list
 
         # Requesting the rarest piece first
-        for count, piece_id in sorted(pieces_by_holder_dict):
+        for count, piece_id in sorted(pieces_by_holder_dict, key=lambda (k,v): k):
 
             # Don't make more requests than the maximum number of requests
             if self.max_requests == len(sent_requests):
@@ -76,14 +77,19 @@ class RanchoStd(Peer):
 
         current_round = history.current_round()
 
-        cooperative_peer_id_list = []
+        cooperative_peers = {}
 
-        if current_round > 0:
-            # Assuming the last round is ok instead of 20 secs.
-            # Other clients might accumulate history of several rounds.
-            recent_download_history = history.downloads[current_round - 1]
-            cooperative_peer_id_list = [(d.blocks, d.from_id) for d in recent_download_history]
+        if current_round > 1:
+            # Since decisions are made every 10 secs, 20 seconds is best represented by two rounds.
+            cooperative_peers = {d.from_id: d.blocks for d in history.downloads[current_round - 1]}
 
+            for download in history.downloads[current_round - 2]:
+                if download.from_id in cooperative_peers:
+                    cooperative_peers[download.from_id] += download.blocks
+                else:
+                    cooperative_peers[download.from_id] = download.blocks
+
+        # Nobody wants our pieces
         if len(incoming_requests) == 0:
             bandwidths = []
         else:
@@ -92,15 +98,35 @@ class RanchoStd(Peer):
             # Requesters shuffled for impartiality
             random.shuffle(requester_id_list)
 
-            cooperative_requester_id_list = sorted(filter(lambda cp: cp[1] in requester_id_list, cooperative_peer_id_list),reverse=True)
+            cooperative_peer_id_list = map(lambda x: x[1], sorted(cooperative_peers.iteritems(), key=lambda (k,v): (v,k), reverse=True))
+
+            # Keeps the order of the coopereative peers from most to least cooperative.
+            cooperative_requester_id_list = filter(lambda cp: cp in requester_id_list, cooperative_peer_id_list)
+
+            # Number of slots not usually optimistically unchoked
+            reciprocative_slots = self.upload_slots - self.optimistic_slots
 
             # Add at most 3 peers by download speed ranking
-            unchoked_peer_id_list = cooperative_requester_id_list[:(self.upload_slots - self.optimistic_slots)]
+            unchoked_peer_id_list = cooperative_requester_id_list[:reciprocative_slots]
 
-            # Use the rest of the slots to unchoke optimistically
+            # Use the rest of the reciprocative slots to unchoke optimistically
             for requester_id in requester_id_list:
-                if len(unchoked_peer_id_list) < 4 and requester_id not in unchoked_peer_id_list:
+                if len(unchoked_peer_id_list) >= reciprocative_slots:
+                    break
+                elif requester_id not in unchoked_peer_id_list:
                     unchoked_peer_id_list.append(requester_id)
+
+            # If the optimistically unchoked peer is not requesting any longer, replace it.
+            if current_round % 3 == 0 or self.optimistically_unchoked_peer_id_list[0] not in requester_id_list:
+                # Optimistically unchoke a peer
+                for requester_id in requester_id_list:
+                    if requester_id not in unchoked_peer_id_list:
+                        unchoked_peer_id_list.append(requester_id)
+                        self.optimistically_unchoked_peer_id_list = [requester_id]
+                        break
+            else:
+                # Unchoke the same agent as in the previous round
+                unchoked_peer_id_list.append(self.optimistically_unchoked_peer_id_list[0])
 
             # Evenly "split" my upload bandwidth among the unchoked requesters
             bandwidths = even_split(self.up_bw, len(unchoked_peer_id_list))
